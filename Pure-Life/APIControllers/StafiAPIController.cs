1a9f8fdfc8f3b5f08566fbe3988f3b37310440c9
Blendhasani <bh51257@ubt-uni.net>
2023-04-25 00:25:07 +0200

    CRUD Staf advanced

---
+++ Pure-Life/APIControllers/StafiAPIController.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Pure_Life.Data;
+using Pure_Life.Models;
+using Pure_Life.ViewModel;
+using Pure_Life.ViewModels;
+
+namespace Pure_Life.APIControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StafiAPIController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public StafiAPIController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        // GET: api/StafiAPI
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Stafi>>> GetStafi()
+        {
+          if (_context.Stafi == null)
+          {
+              return NotFound();
+          }
+            return await _context.Stafi.ToListAsync();
+        }
+
+        // GET: api/StafiAPI/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Stafi>> GetStafi(int id)
+        {
+          if (_context.Stafi == null)
+          {
+              return NotFound();
+          }
+            var stafi = await _context.Stafi.FindAsync(id);
+
+            if (stafi == null)
+            {
+                return NotFound();
+            }
+
+            return stafi;
+        }
+
+        // PUT: api/StafiAPI/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutStafi(int id, Stafi stafi)
+        {
+            if (id != stafi.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(stafi).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!StafiExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/StafiAPI
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Stafi>> PostStafi(Stafi stafi)
+        {
+          if (_context.Stafi == null)
+          {
+              return Problem("Entity set 'ApplicationDbContext.Stafi'  is null.");
+          }
+            _context.Stafi.Add(stafi);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetStafi", new { id = stafi.Id }, stafi);
+        }
+
+		
+		[HttpPost("{email}/{password}")]
+		public async Task<ActionResult<Stafi>> GetStafiByEmailAndPassword(string email, string password)
+		{
+            var stafi = new Stafi();
+			if (email == null || password == null)
+			{
+				return Problem("Email or password is null.");
+			}
+
+			var user = await _userManager.FindByEmailAsync(email);
+            
+			if (user == null)
+			{
+				return NotFound();
+            }
+            else
+            {
+				var passwordCheck = await _userManager.CheckPasswordAsync(user, password);
+				if (!passwordCheck)
+				{
+					return NotFound();
+				}
+				else
+				{
+					stafi = await _context.Stafi.FirstOrDefaultAsync(x => x.Email == user.Email);
+				}
+
+			}
+
+
+
+			var staf = new Stafi
+			{
+				Id = stafi.Id,
+				NrLeternjoftimit = stafi.NrLeternjoftimit,
+				Emri = stafi.Emri,
+				Mbiemri = stafi.Mbiemri,
+				Gjinia = stafi.Gjinia,
+				DataLindjes = stafi.DataLindjes,
+				NrLincences = stafi.NrLincences,
+				NrTel = stafi.NrTel,
+				PictureUrl = stafi.PictureUrl,
+				PublicId = stafi.PublicId,
+				RoletId = stafi.RoletId,
+				ShtetiId = stafi.ShtetiId,
+				Qyteti = stafi.Qyteti,
+				NacionalitetiId = stafi.NacionalitetiId,
+				LemiaId = stafi.LemiaId,
+				Email = stafi.Email,
+				EmailZyrtar = stafi.EmailZyrtar,
+				InsertedFrom = stafi.InsertedFrom,
+				InsertedDate = stafi.InsertedDate,
+				ModifiedFrom = stafi.ModifiedFrom,
+				ModifiedDate = stafi.ModifiedDate
+			};
+
+			return staf;
+
+		}
+
+
+		// DELETE: api/StafiAPI/5
+		[HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteStafi(int id)
+        {
+            if (_context.Stafi == null)
+            {
+                return NotFound();
+            }
+            var stafi = await _context.Stafi.FindAsync(id);
+            if (stafi == null)
+            {
+                return NotFound();
+            }
+
+            _context.Stafi.Remove(stafi);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool StafiExists(int id)
+        {
+            return (_context.Stafi?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Pure-Life/ViewModel/Stafi/EditStafiViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Pure_Life.ViewModel.Stafi
+{
+	public class EditStafiViewModel
+	{
+		public int Id { get; set; }
+		public string? NrLeternjoftimit { get; set; }
+		public string Emri { get; set; }
+		public string Mbiemri { get; set; }
+		public string? Gjinia { get; set; }
+		public DateTime? DataLindjes { get; set; }
+		public string? NrLincences { get; set; }
+		public string? NrTel { get; set; }
+		public int RoletId { get; set; }
+		public string Email { get; set; }
+		public string EmailZyrtar { get; set; }
+		public int? ShtetiId { get; set; }
+		public string? Qyteti { get; set; }
+		public int? NacionalitetiId { get; set; }
+		public int LemiaId { get; set; }
+		public DateTime ModifiedDate { get; set; }
+	    public string ModifiedFrom { get; set; }
+
+
+		[Required]
+		[DataType(DataType.Password)]
+		public string Password { get; set; }
+
+		[Display(Name = "Confirm password")]
+		[Required(ErrorMessage = "Confirm password is required")]
+		[DataType(DataType.Password)]
+
+		[Compare("Password", ErrorMessage = "Passwords do not match")]
+		public string ConfirmPassword { get; set; }
+
+		public IFormFile PictureUrl { get; set; }
+	}
+}
