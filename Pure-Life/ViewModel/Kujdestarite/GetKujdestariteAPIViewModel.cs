9e2a2996b6738af81c5f3d8ccd2bfcaa19768f98
Blendhasani <bh51257@ubt-uni.net>
2023-05-27 15:22:43 +0200

    Some changes

---
+++ Pure-Life/ViewModel/Kujdestarite/GetKujdestariteAPIViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Pure_Life.ViewModel.Kujdestarite
+{
+	public class GetKujdestariteAPIViewModel
+	{
+		public int Id { get; set; }
+		public DateTime Data { get; set; }
+
+		public int Kati { get; set; }
+		public string Reparti { get; set; }
+
+		public string StafiEmriMbiemri { get; set; }
+		public int StafiId { get; set; }
+	}
+}
