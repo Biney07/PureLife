bdd03cf55662bf47bb8acd840c13ab60923022b2
Blendhasani <bh51257@ubt-uni.net>
2023-06-15 13:25:17 +0200

    Perfundimi i metodave te terapive

---
+++ Pure-Life/ViewModel/Terapia/EditTerapiaViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Pure_Life.ViewModel.Terapia
+{
+	public class EditTerapiaViewModel
+	{
+		public string Pershkrimi { get; set; }
+		public string Diagnoza { get; set; }
+
+		public string Barnat { get; set; }
+		public string SherbimetEKryera { get; set; }
+
+	}
+}
