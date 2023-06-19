4fe1d5cd787e94e60c28c9b9b77a6e2b4df8cadb
Blendhasani <bh51257@ubt-uni.net>
2023-06-19 18:50:22 +0200

    OpenAI Integration in backend and frontend

---
+++ Pure-Life/APIControllers/OpenAIAPIController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OpenAI_API;
+using OpenAI_API.Completions;
+
+namespace ChatGPT_CSharp.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class OpenAIAPIController : ControllerBase
+	{
+		[HttpGet("use-chat")]
+		public async Task<IActionResult> UseChatGPT(string query)
+		{
+			string outputResult = "";
+			var openai = new OpenAIAPI("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");
+			CompletionRequest completionRequest = new CompletionRequest();
+			completionRequest.Prompt = query+" , te mjeku i ciles lemi duhet te shkoj?";
+			completionRequest.Model = OpenAI_API.Models.Model.DavinciText;
+
+			int promptTokens = CountTokens(query);
+			int maxCompletionTokens = 4096;
+
+			completionRequest.MaxTokens = maxCompletionTokens -150;
+
+			var completions = await openai.Completions.CreateCompletionAsync(completionRequest);
+
+			foreach (var completion in completions.Completions)
+			{
+				outputResult += completion.Text;
+			}
+
+			return Ok(outputResult);
+		}
+
+		private int CountTokens(string text)
+		{
+			// Replace this logic with your own tokenization method or library
+			string[] tokens = text.Split(' ');
+			return tokens.Length;
+		}
+
+
+
+
+
+	}
+}
---
+++ Pure-Life/APIControllers/StafiDashboardAPIController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Pure_Life.Data;
+
+namespace Pure_Life.APIControllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class StafiDashboardAPIController : ControllerBase
+	{
+
+		private readonly ApplicationDbContext _context;
+
+		public StafiDashboardAPIController(ApplicationDbContext context)
+		{
+			_context= context;
+		}
+
+		[HttpGet("NumriTotalIPacienteve/{stafiId}")]
+		public int NumriTotalIPacienteve(int stafiId)
+		{
+			var pacientet = _context.Pacientet.Select(x => x.Id).ToList();
+			var count = _context.Terminet.Where(x => x.StafiId == stafiId && pacientet.Contains(x.Pacienti.Id)).Select(x => x.Pacienti.Id).Distinct().Count();
+			return count;
+
+		}
+	}
+}
