e656bb004cec7f84f0f6a3979cf8c45edec1cf35
Blendhasani <bh51257@ubt-uni.net>
2023-06-24 00:18:02 +0200

    Numri i termineve per muaj per stafin specifik

--- Pure-Life/APIControllers/StafiDashboardAPIController.cs
+++ Pure-Life/APIControllers/StafiDashboardAPIController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using OpenAI_API.Moderation;
 using Pure_Life.Data;
 using Pure_Life.ViewModel.Stafi;
 
@@ -39,7 +40,87 @@ namespace Pure_Life.APIControllers
 
 			return Ok(result);	
 
-        }	
+        }
+
+		[HttpGet("TerminiMuaji/{stafiId}")]
+
+		public async Task<IActionResult> TerminiMuaji (int stafiId) 
+		{
+
+            var terminetEStafit = await _context.Terminet.Where(x => x.Price > 0 && x.StafiId == stafiId).ToListAsync();
+            var terminetEPerfunduara = await _context.Terminet.CountAsync(x => x.Price > 0 && x.StafiId == stafiId);
+
+			List<int> muajt = new List<int>();
+
+			for(var i = 1;i<=12;i++)
+			{
+				muajt.Add(i);
+			}
+
+   
+
+
+            var result = new TerminetMuajiViewModel();
+              List<TerminetMuajiViewModel> results = new List<TerminetMuajiViewModel>();
+
+            foreach (var muaji in muajt)
+			{
+                var monthName = string.Empty;
+                switch (muaji)
+                {
+                    case 1:
+                        monthName = "Janar";
+                        break;
+                    case 2:
+                        monthName = "Shkurt";
+                        break;
+                    case 3:
+                        monthName = "Mars";
+                        break;
+                    case 4:
+                        monthName = "Prill";
+                        break;
+                    case 5:
+                        monthName = "Maj";
+                        break;
+                    case 6:
+                        monthName = "Qershor";
+                        break;
+                    case 7:
+                        monthName = "Korrik";
+                        break;
+                    case 8:
+                        monthName = "Gusht";
+                        break;
+                    case 9:
+                        monthName = "Shtator";
+                        break;
+                    case 10:
+                        monthName = "Tetor";
+                        break;
+                    case 11:
+                        monthName = "Nëntor";
+                        break;
+                    case 12:
+                        monthName = "Dhjetor";
+                        break;
+                    default:
+                        monthName = "Invalid month number";
+                        break;
+                }
+                result = new TerminetMuajiViewModel()
+                {
+                    NumriTermineveTePerfunduara = terminetEStafit.Select(x => DateTime.TryParse(x.StartTime, out DateTime startTime) && startTime.Month == muaji && x.Price > 0).Count() == 0 ? 0 : terminetEStafit.Count(x => DateTime.TryParse(x.StartTime, out DateTime startTime) && startTime.Month == muaji && x.Price > 0),
+                    Muaji = monthName
+                };
+
+                results.Add(result);
+            }
+
+            return Ok(results);
+
+        }
+        
 			
 	}
 }
---
+++ Pure-Life/ViewModel/Stafi/TerminetMuajiViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Pure_Life.ViewModel.Stafi
+{
+    public class TerminetMuajiViewModel
+    {
+        public string Muaji { get; set; }
+
+       public int? NumriTermineveTePerfunduara { get; set; }
+
+    }
+
+
+}
